8566ee4b74431e59b1eaee1e2475af9626fbc284
Jackson Mades <jacksonmades@gmail.com>
2019-11-06 14:02:58 -0500

    lab 8 and more trees tard

---
+++ 26 more trees tard.rkt
@@ -0,0 +1,36 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname |26 more trees tard|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+(define (bst-temp abst)
+  (cond [(string? abst) ...]
+        [(branch? abst) ... (branch-key abst)
+                        ...(branch-info abst)
+                        ...(bst-temp (branch-left abst))
+                        ...(bst-temp (branch-right abst))]))
+
+
+;;retrieve: BST NatNum -> String
+;get the info at the given key in the tree
+(define (retrieve abst akey)
+  (cond [(string? abst) (error "key not found")]
+        [(branch? abst) (cond [(= akey (branch-key abst)) (branch-info abst)]
+                              [(< akey (branch-key abst))
+                               (retrieve (branch-left abst) akey)]
+                              [else (retrieve (branch-right abst)akey)])]))
+
+;bst->list: BST -> [List-of NatNum]
+;preoduces a list of the keys in the tree
+(check-expect (bst->list bst1) (list 7 8 10 12 13 14))
+(check-expect (bst->list "leaf") empty)
+
+(define (bst->list abst)
+  (cond [(string? abst) empty]
+        [(branch? abst) (append (list (branch-key abst))
+                                (bst->list (branch-left abst))
+                                (bst->list (branch-right abst)))]))
+
+;bst? : BST -> Boolean
+;is the tree a valid bst?
+(check-expect (bst? bst1) true)
+(check-expect (bst? bst1-bad) false)
+(check-expect (bst? "leaf") true)
--- Lab/lab 8.rkt
+++ Lab/lab 8.rkt
@@ -21,3 +21,29 @@
          (cons (first l1)
                (alternate.v2 l2 (rest l1)))]))
 ;;swicthes the args back and forth to make in leu of an actual code choosing back and forth
+
+
+;ex 2
+;cross-product: [List-of X] [List-of Y] -> [List-of (list X Y)]
+;length of result list = length of lox * length of loy
+(define (cross-product lox loy)
+  (cond [(empty? lox) empty]
+        [(cons? lox) (append
+                      (do-one-cross-product (first lox) loy)
+                     (cross-product (rest lox) loy))]))
+
+(define (crs-prd-2 lox loy)
+  (foldr (λ (current sofar) (append (do-one-cross-product current loy) empty lox))
+         empty lox))
+
+;do-one-cross-product: X [List-of Y] -> [List-of (list X Y)]
+;pairs x with each element in the list
+;turning a list i nto another list of the same length
+(define (do-one-cross-product x loy)
+  (cond [(empty? loy) empty]
+        [(cons? loy) (cons (list x (first loy)) ; this is the first element
+                     (do-one-cross-product x (rest loy)))]))
+
+(define (do-one-cross-product x loy)
+  (map (λ (element-of-the-list) (list x element-of-the-list)) loy))
+
